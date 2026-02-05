0ade9539d0324402ad4fbfb68495c16e9eb2e3b4
discpic-ron <granddriod1210@gmail.com>
2026-02-05 16:43:02 -0500

    Refactor Item class constructor parameters
    Renamed 'classification' parameter to 'item_type' and added 'is_weapon' attribute.

--- source/item.py
+++ source/item.py
@@ -1,10 +1,11 @@
 class Item:
-    def __init__(self, name, description, damage, classification):
+    def __init__(self, name, description, damage, item_type):
         self.name = name
         self.description = description
         self.damage = damage
-        self.classification = classification
-
+        self.item_type = item_type
+        self.is_weapon = False
+    
     def getName(self):
         return self.name
 
