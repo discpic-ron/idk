85d466f3edb7dc570b97d618f471e6c93575f326
discpic-ron <granddriod1210@gmail.com>
2026-02-05 19:50:21 -0500

    Refactor honor management methods in Privilege class

--- source/privlages.py
+++ source/privlages.py
@@ -1,27 +1,27 @@
 class Privilege:
   privilege_list = []
+  
   def __init__(self, name, threshold):
     self.name = name
     self.threshold = threshold
     self.status = "locked"
-  def update_status(self, honor_amount):
-    if honor_amount >= self.threshold and self.status == "locked":
+    
+  def update_status(self,current_honor):
+    if current_honor >= self.threshold and self.status == "locked":
       self.status = "unlocked"
       if self.name not in privileges:
         privileges.append(self.name)
-      print(f"{self.name} unlocked at {honor_amount} xp!")
+      print(f"{self.name} unlocked at {current_honor} xp!")
       
-  def grant_honor(self,amount):
-    global current_honor
+  def grant_honor(self,amount,current_honor):
     current_honor += amount
-    if current_honor > max_honor:
-      current_honor = max_honor
+    if current_honor > self.threshold:
+      current_honor = self.threshold
     for p in Privilege.privilege_list:
       p.update_status(current_honor)
     return current_honor, privileges
   
-  def lose_honor(self, amount):
-    global current_honor
+  def lose_honor(self, amount, current_honor):
     current_honor -= amount
     if current_honor < 0:
       current_honor = 0
