b03506fdbb7bde8ab5d04412ce6b718e9d7ed2e2
discpic-ron <granddriod1210@gmail.com>
2026-02-05 19:38:59 -0500

    Implement honor management in Privilege class
    Added methods to grant and lose honor, updating privileges accordingly.

--- source/privlages.py
+++ source/privlages.py
@@ -1,12 +1,35 @@
 class Privilege:
+  privilege_list = []
   def __init__(self, name, threshold):
     self.name = name
     self.threshold = threshold
     self.status = "locked"
-
   def update_status(self, honor_amount):
     if honor_amount >= self.threshold and self.status == "locked":
       self.status = "unlocked"
       if self.name not in privileges:
         privileges.append(self.name)
       print(f"{self.name} unlocked at {honor_amount} xp!")
+      
+  def grant_honor(self,amount):
+    global current_honor
+    current_honor += amount
+    if current_honor > max_honor:
+      current_honor = max_honor
+    for p in Privilege.privilege_list:
+      p.update_status(current_honor)
+    return current_honor, privileges
+  
+  def lose_honor(self, amount):
+    global current_honor
+    current_honor -= amount
+    if current_honor < 0:
+      current_honor = 0
+    # revoke privileges if honor drops below threshold
+    for p in Privilege.privilege_list:
+      if current_honor < p.threshold and p.status == "unlocked":
+        p.status = "locked"
+        if p.name in privileges:
+          privileges.remove(p.name)
+        print(f"{p.name} revoked at {current_honor} Hxp!")
+    return current_honor, privileges
