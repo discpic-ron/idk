90367a5e33782f044dfb4d9c1a50b3a4a0ad039e
discpic-ron <granddriod1210@gmail.com>
2026-03-10 19:19:32 -0400

    Refactor Weapon class to inherit from Item

--- source/weapon.py
+++ source/weapon.py
@@ -1,9 +1,31 @@
-class Weapon:
-    def __init__(self):
-        self.ammo = 0
+from item import Item
 
-    def shoot(self):
-        self.ammo -= 1
+class Weapon(Item):
+  def __init__(self,name,damage,durability,weight,animation=None,range=None,cool_down=0,stackable=False,max_stack=1):
+    super().__init__(name,stackable,max_stack)
+    self.name = name
+    self.damage = damage
+    self.max_durability = 100
+    self.durability = self.max_durability
+    self.range = range
+    self.cool_down = cool_down
+    self.last_used = 0
+    self.weight = weight
+    self.status_effect = "poison"
+    self.status_duration = 5
+    self.animation = animation
+    self.broken = False
 
-    def reload(self, amount):
-        self.ammo += amount
+  def use(self,amount):
+    if self.durability > 0:
+      self.durability -= amount
+
+  def repair(self,amount):
+    if self.durability < 100:
+      self.durability += amount
+
+  def can_use(self, current_time):
+    return current_time - self.last_used >= self.cooldown
+
+  def apply_effect(self):
+    pass
