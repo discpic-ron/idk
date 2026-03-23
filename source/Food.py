e710bd477e891fc381ca47077c6aff54ce9ceabf
discpic-ron <granddriod1210@gmail.com>
2026-03-23 16:27:30 -0400

    Create Food.py

---
+++ source/Food.py
@@ -0,0 +1,27 @@
+import pygame
+from Item import item
+
+class food(item):
+    def __init__(self, name, image,food_value, effect=None, stackable=True, max_stack=99):
+        super().__init__(name, stackable, max_stack)
+        self.name = name
+        self.image = image
+        self.food_value = food_value
+        self.effect = effect
+
+    def apply(self, player):
+        if self.effect == "hunger":
+            player.hunger += self.food_value
+
+        elif self.effect == "thirst":
+            player.thirst += self.food_value
+
+    def draw(self):
+        pass
+
+    def get_info(self):
+        return {
+            "name": self.name,
+            "effect": self.effect,
+            "value": self.food_value
+        }
