53f2b103ae05161b6a8b5869e7da4f1ca253d848
discpic-ron <granddriod1210@gmail.com>
2026-03-23 16:26:50 -0400

    Create Inventory.py

---
+++ source/Inventory.py
@@ -0,0 +1,15 @@
+import pygame
+
+class inventory:
+    def __init__(self, size):
+        self.size = size
+        self.slots = [None] * size
+
+    def add_item(self,item,amount=1):
+        pass
+
+    def remove_item(self, name, amount=1):
+        pass
+    
+    def draw(self,screen):
+        pass
