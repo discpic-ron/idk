30380e06e6cfbd0ece70a70737bcb0aa09b6bb66
discpic-ron <granddriod1210@gmail.com>
2026-03-22 22:15:33 -0400

    Update item.py

--- source/item.py
+++ source/item.py
@@ -1,13 +1,24 @@
-class Item:
-    def __init__(self, name, description, damage, item_type):
-        self.name = name
-        self.description = description
-        self.damage = damage
-        self.item_type = item_type
-        self.is_weapon = False
-    
-    def getName(self):
-        return self.name
+class item:
 
-    def getDescription(self):
-        return self.description
+    def __init__(self, name, stackable=True, max_stack=99,weight=1.0):
+        self.name = name
+        self.stackable = stackable
+        self.max_stack = max_stack
+        self.weight = weight
+        self.current_stack = 0
+
+    def add_stack(self):
+        if self.current_stack <= self.max_stack:
+            self.current_stack += 1
+            return self.current_stack
+
+    def remove_stack(self):
+        if self.current_stack <= self.max_stack:
+            self.current_stack -= 1
+            return self.current_stack
+
+    def get_info(self):
+        return {
+            "name": self.name,
+            "count": self.current_stack
+        }
